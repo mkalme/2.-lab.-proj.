dc6fbf8dfc2c1c098c7638db90b60b134490347e
mkalme <mikelis.kalme@gmail.com>
2025-05-22 18:46:51 +0300

    Add project files.

---
+++ 2. lab. proj/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _2._lab._proj_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
